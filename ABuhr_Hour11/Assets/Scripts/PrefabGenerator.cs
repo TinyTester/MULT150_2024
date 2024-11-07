86347c9113b17bc06343187bee154fdfd1dfb4e1
TinyTester <tiny.tester.0@gmail.com>
2024-11-07 11:03:59 -0800

    Finished Hour 11

--- ABuhr_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ ABuhr_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    int AAA = -10;
    
-   
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            transform.position = new Vector3( AAA, 0, 0);
+            AAA += 3;
+        }
+        
+
+    }
    
     // Update is called once per frame
     void Update()
