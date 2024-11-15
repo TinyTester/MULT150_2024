7a9e7e29f22feb8c74a57fe88cb07f988fefd0e5
TinyTester <tiny.tester.0@gmail.com>
2024-11-15 13:38:04 -0800

    Completed Hour 16

---
+++ ABuhr_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+Particle effect changes
+- Changed emitter shape to hemisphere
+- Added trails to particles and changed their texture
+- Added gravity
+- Changed color to yellow
+- Made particles change size as they decay
+*/
