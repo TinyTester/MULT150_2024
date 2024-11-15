2438df8970662f4c361f49e5958957b2fc48201f
TinyTester <tiny.tester.0@gmail.com>
2024-11-15 09:18:14 -0800

    Completed Hour 14 (but NOT 16)

---
+++ ABuhr_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,11 @@
+/*
+User interface changes:
+- Changed UI from title screen to in-game menu
+    - Removed title, description, and start button
+- Changed background image to placeholder for actual game screen
+- added pinned quest box with placeholder quests
+- Added notebook and settings buttons
+- all buttons lead to placeholder menu/scene bout would all lead to their own menus in a complete game
+- Placeholder scene/menu now has a back button to starting screen
+
+*/
