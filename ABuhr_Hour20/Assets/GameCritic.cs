15025fd9ebd6546abd8798bb48928e69f5a060e7
TinyTester <tiny.tester.0@gmail.com>
2024-12-04 13:55:25 -0800

    Completed Hour20

---
+++ ABuhr_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,42 @@
+/*
+ ________  ________  _____ ______   _______           ________  ________  ___  _________  ___  ________     
+|\   ____\|\   __  \|\   _ \  _   \|\  ___ \         |\   ____\|\   __  \|\  \|\___   ___\\  \|\   ____\    
+\ \  \___|\ \  \|\  \ \  \\\__\ \  \ \   __/|        \ \  \___|\ \  \|\  \ \  \|___ \  \_\ \  \ \  \___|    
+ \ \  \  __\ \   __  \ \  \\|__| \  \ \  \_|/__       \ \  \    \ \   _  _\ \  \   \ \  \ \ \  \ \  \       
+  \ \  \|\  \ \  \ \  \ \  \    \ \  \ \  \_|\ \       \ \  \____\ \  \\  \\ \  \   \ \  \ \ \  \ \  \____  
+   \ \_______\ \__\ \__\ \__\    \ \__\ \_______\       \ \_______\ \__\\ _\\ \__\   \ \__\ \ \__\ \_______\
+    \|_______|\|__|\|__|\|__|     \|__|\|_______|        \|_______|\|__|\|__|\|__|    \|__|  \|__|\|_______|
+
+
+--- QUESTIONS ---
+
+Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+    I thinnk that the game is a bit too hard. The main two reasons are that the obstacles move too fast. 
+    The other reason is that the player takes time to build up speed when changing directions, making it harder to dodge obstacles.
+
+What 2 changes would you do to make it easier or harder?
+    I would slow down the incoming objects and make the player's movement a bit more reactive.
+
+List 2 things that would give it a "wow" factor.
+    I think that syncing the "movement" of the ground and the speed of the incoming objects would be a nice touch. 
+    I also think adding in more types of powerups and obstacles would be fun.
+    You could also add increasing difficulty as time goes on.
+
+What 2 parts of the game are fun?
+    It's fun trying to catch the powerups. 
+    It's also fun watching yourself phase through the obstacles.
+
+What 2 parts of the game are tedious?
+    Dodging the obstacles is kind of annoying. The lack of change in difficulty and obstacles in also a bit tedious.
+
+------------------
+
+--- MODIFIER NOTES ---
+
+Modify the obstacle prefab (Colour, model, anything! The red cube is pretty boring)
+    Added a frowny face to the obstacles
+Modify the powerup prefab (Rework the particle system component. It doesn't need to be perfect, just have fun with it!)
+    Added a lightning trail between the emmitted particles
+
+
+*/
