fb8aed443a94a5bd63bea893eed0d7e60514ceb1
TinyTester <tiny.tester.0@gmail.com>
2024-12-10 11:03:02 -0800

    Completed Final Assignment

---
+++ ABuhr_Final/Assets/Modifications.cs
@@ -0,0 +1,44 @@
+/*
+
+88b           d88                      88 88    ad88 88                               88                                    
+888b         d888                      88 ""   d8"   ""                         ,d    ""                                    
+88`8b       d8'88                      88      88                               88                                          
+88 `8b     d8' 88  ,adPPYba,   ,adPPYb,88 88 MM88MMM 88  ,adPPYba, ,adPPYYba, MM88MMM 88  ,adPPYba,  8b,dPPYba,  ,adPPYba,  
+88  `8b   d8'  88 a8"     "8a a8"    `Y88 88   88    88 a8"     "" ""     `Y8   88    88 a8"     "8a 88P'   `"8a I8[    ""  
+88   `8b d8'   88 8b       d8 8b       88 88   88    88 8b         ,adPPPPP88   88    88 8b       d8 88       88  `"Y8ba,   
+88    `888'    88 "8a,   ,a8" "8a,   ,d88 88   88    88 "8a,   ,aa 88,    ,88   88,   88 "8a,   ,a8" 88       88 aa    ]8I  
+88     `8'     88  `"YbbdP"'   `"8bbdP"Y8 88   88    88  `"Ybbd8"' `"8bbdP"Y8   "Y888 88  `"YbbdP"'  88       88 `"YbbdP"'  
+
+Chose: GAUNTLET RUNNER
+
+    1️⃣ directional light (colour, intensity, etc)
+        Slightly changed direction, changed the color to pink, changed the intensity to 1.5
+
+    2️⃣ fog
+        Added black fog
+
+    3️⃣ skybox (atmosphere thickness, sky tint, exposure, etc)
+        Changed the skybox texture to this image: https://www.pexels.com/photo/starry-sky-998641/
+
+    obstacle prefab
+        4️⃣ convert to complex object comprised of 3 or more shapes (Hints: Spikes, marbles, rocks, pile of crates, etc)
+            Made it an angry robot
+        5️⃣ use gameobject hierarchy to keep it organised
+            Made folders to hold the different related parts
+        6️⃣ apply at least 2 new textures
+            Got a scratched metal texture from https://lostandtaken.com/downloads/scratched-scraped-metal-texture-17/
+            And a spotted tecture from https://lostandtaken.com/downloads/focus-bokeh-texture-24/
+        7️⃣ add particle system component and customise at least 3 parameters
+            Added particle system with trails, custom colors, and physics
+
+    powerup prefab
+        8️⃣ modify light component (colour, range, intensity, etc)
+            Shrank the range and added a halo effect
+    wall
+        9️⃣ convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+            Completed
+        🔟 modify wall material tint colour so that the 2 walls are different colours (Hints: This may involve using 2 separate wall materials)
+            Made the second wall blue
+
+
+*/
